ff38b1911b54c942a28e118574d8ee9c7581d813
Chinmay Shringi <31031919+ChinmayShringi@users.noreply.github.com>
2021-06-27 17:53:08 +0530

    connect init

---
+++ textSpreadConnect/scripts.js
@@ -0,0 +1,101 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let particleArray = [];
+let adjustX = 10;
+let adjustY = 10;
+// handle mouse
+const mouse = {
+  x: null,
+  y: null,
+  radius: 150,
+};
+
+window.addEventListener("mousemove", (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+ctx.fillStyle = "white";
+ctx.font = "30px Verdana";
+ctx.fillText("A", 0, 40);
+ctx.strokeStyle = "white";
+// ctx.strokeRect(0, 0, 100, 100);
+const textCoordinates = ctx.getImageData(0, 0, 100, 100);
+
+class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 3;
+    this.baseX = this.x;
+    this.baseY = this.y;
+    this.density = Math.random() * 40 + 5;
+  }
+  draw() {
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+  }
+  update() {
+    let dx = mouse.x - this.x;
+    let dy = mouse.y - this.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+    let forceDirectionX = dx / distance;
+    let forceDirectionY = dy / distance;
+    let maxDistance = mouse.radius;
+    let force = (maxDistance - distance) / maxDistance;
+    let directionX = forceDirectionX * force * this.density;
+    let directionY = forceDirectionY * force * this.density;
+    if (distance < mouse.radius) {
+      this.x -= directionX;
+      this.y -= directionY;
+    } else {
+      if (this.x !== this.baseX) {
+        let dx = this.x - this.baseX;
+        this.x -= dx / 10;
+      }
+      if (this.y !== this.baseY) {
+        let dy = this.y - this.baseY;
+        this.y -= dy / 10;
+      }
+    }
+  }
+}
+
+function init() {
+  particleArray = [];
+  for (let y = 0, y2 = textCoordinates.height; y < y2; y++) {
+    for (let x = 0, x2 = textCoordinates.width; x < x2; x++) {
+      // check for opacity
+      if (
+        textCoordinates.data[y * 4 * textCoordinates.width + x * 4 + 3] > 128
+      ) {
+        let postitionX = x + adjustX;
+        let postitionY = y + adjustY;
+        particleArray.push(new Particle(postitionX * 10, postitionY * 10));
+      }
+    }
+  }
+
+  // for (let i = 0; i < 10; i++) {
+  //   let x = Math.random() * 500;
+  //   let y = Math.random() * 500;
+  //   particleArray.push(new Particle(x, y));
+  // }
+  // particleArray.push(new Particle(50, 50));
+  // particleArray.push(new Particle(80, 50));
+}
+init();
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].draw();
+    particleArray[i].update();
+  }
+  requestAnimationFrame(animate);
+}
+animate();
